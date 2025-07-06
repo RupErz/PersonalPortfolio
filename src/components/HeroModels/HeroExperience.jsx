ddd034b42e635922d8fcb202a81eab5de72758e6
Nghia <nghiavu144@gmail.com>
2025-07-06 12:54:41 -0500

    Finish 3d model

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -2,6 +2,9 @@ import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import { useMediaQuery } from 'react-responsive'
+import { Room } from './Room'
+import HeroLights from './HeroLights'
+import Particles from './Particles'
 
 const HeroExperience = () => {
     const isTablet = useMediaQuery({ query: '(max-width: 1024px)' })
@@ -9,9 +12,6 @@ const HeroExperience = () => {
 
     return (
         <Canvas camera={{position: [0, 0, 15], fov: 45}}>
-            <ambientLight intensity={0.2} color="#1a1a40"/>
-            <directionalLight position={[5, 5, 5]} intensity={5} />
-
             <OrbitControls 
                 enablePan={false}
                 enableZoom={!isTablet}
@@ -21,10 +21,17 @@ const HeroExperience = () => {
                 maxPolarAngle={Math.PI / 2}
             />
 
-            <mesh>
-                <boxGeometry args={[1, 1, 1]}/>
-                <meshStandardMaterial color="blue"/>
-            </mesh>
+            <HeroLights />
+
+            {/* Set number of particles falling */}
+            <Particles count={50} />
+            <group
+                scale={isMobile ? 0.7 : 1}
+                position={[0, -3.5, 0]}
+                rotation={[0, -Math.PI / 4, 0]}
+            >
+                <Room />
+            </group>
         </Canvas>
     )
 }
